6b31028d800103d528e096b613d49294184f9146
Backflip96 <arthurbogil94@gmail.com>
2013-10-03 22:35:50 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<title>DX-Informatica, Votre partenaire informatique.</title>
+<link rel="stylesheet" href="index.css" />
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf-8">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="LibreOffice 4.1.0.4 (Linux)">
+	<META NAME="AUTHOR" CONTENT="root ">
+	<META NAME="CREATED" CONTENT="20130812;171103693483819">
+	<META NAME="CHANGEDBY" CONTENT="root ">
+	<META NAME="CHANGED" CONTENT="20130812;173307614471111">
+</HEAD>
+<BODY LANG="fr-BE" DIR="LTR">
+<div id="bloc_page">
+<BR><BR>
+<header>
+<div id="titre_principal"><img src="images/ordi.ico" alt="Logo" id="logo" />
+<h1>DX-Informatica,</h1></P>
+<p><h2>Votre partenaire informatique.</h2></div>
+<BR><BR>
+<nav><ul>
+<li><A HREF="http://www.dx-informatica.comxa.com/#" NAME="Accueil">Accueil</A></li>
+<li><a href="/infos.html" name="infos">Infos</a></li>
+<li><a href="/services.html" Name="Services">Services</a></li>
+<li><a href="/contacts.html" name="contacts">Contacts</a></li>
+<li><a href="http://dx-informatica-forum.comyr.com" name="forum">Forum</a></li>
+</ul></nav>
+</header>
+<div id="banniere_image">
+<div id="banniere_description">
+Nos Tarifs ...
+<a href="/infos.html" class="bouton_rouge">Voir l'article
+<img src="images/flecheblanchedroite.png" alt=" " /></a>
+</div>
+</div>
+<section>
+<article>
+<h1><img src="images/ico_epingle.png" alt="Categorie voyage" class="ico_categorie" />Accueil</h1>
+<UL>
+	<p><b>Bienvenue</b></P>
+</UL>
+<P>Bienvenue sur le site web de DX-Informatica.</br ></br >
+Découvrez nos nouvelles rubriques, nos services ou encore notre forum.</br ></br >
+Découvrez également nos prix exceptionnels dans notre rubrique : "<a href="/services.html" Name="Services">Services</a>"</p>
+</br >
+</br >
+</article>
+<aside>
+<p id="photo_zozor"><img src="images/iconedx.png" alt="Logo DX" /></p>
+</aside>
+</section>
+<footer>
+<nav><p>Webmaster : Dorian Braeckman </br>
+Copyright &copy;2013, DX-Informatica est une marque d&eacute;pos&eacute;e.</p></footer></html>
+<!-- Hosting24 Analytics Code -->
+<script type="text/javascript" src="http://stats.hosting24.com/count.php"></script>
+<!-- End Of Analytics Code -->
